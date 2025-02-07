8c589c2aefcad43468b8fe3d649dc8bc96abe790
Tom Jooris <tjooris@z1r4p5.42lyon.fr>
2025-02-07 15:19:08 +0100

    ENFIN FINI

--- .make/build_main/checker_bonus/reverse_rotate.d
+++
@@ -1,7 +0,0 @@
-.make/build_main/checker_bonus/reverse_rotate.o: \
-  src/checker_bonus/reverse_rotate.c include/push_swap.h \
-  lib/libft/include/libft.h
-
-include/push_swap.h:
-
-lib/libft/include/libft.h:
--- .make/build_main/checker_bonus/rotate.d
+++
@@ -1,6 +0,0 @@
-.make/build_main/checker_bonus/rotate.o: src/checker_bonus/rotate.c \
-  include/push_swap.h lib/libft/include/libft.h
-
-include/push_swap.h:
-
-lib/libft/include/libft.h:
