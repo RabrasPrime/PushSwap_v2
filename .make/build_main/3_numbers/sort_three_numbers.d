c51985a9fb8a2ea2eeaace5623660f789b2d6f15
Tom Jooris <tjooris@z1r2p1.42lyon.fr>
2025-02-19 18:10:29 +0100

    all

---
+++ .make/build_main/3_numbers/sort_three_numbers.d
@@ -0,0 +1,9 @@
+.make/build_main/3_numbers/sort_three_numbers.o: \
+  src/3_numbers/sort_three_numbers.c include/struct.h include/stack.h \
+  include/move.h
+
+include/struct.h:
+
+include/stack.h:
+
+include/move.h:
---
+++ .make/build_main/big_numbers/do_moves.d
@@ -0,0 +1,6 @@
+.make/build_main/big_numbers/do_moves.o: src/big_numbers/do_moves.c \
+  include/struct.h include/move.h
+
+include/struct.h:
+
+include/move.h:
