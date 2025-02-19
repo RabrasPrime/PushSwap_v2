e4d61a013bca4e80115e6b7630b46f53e4effbed
Tom Jooris <tjooris@z4r2p2.42lyon.fr>
2025-02-19 17:57:18 +0100

    all

--- .make/build_main/arg/arg_handler.d
+++
@@ -1,8 +0,0 @@
-.make/build_main/arg/arg_handler.o: src/arg/arg_handler.c \
-  lib/libft/include/libft.h include/stack.h include/struct.h
-
-lib/libft/include/libft.h:
-
-include/stack.h:
-
-include/struct.h:
--- .make/build_main/moves/reverse_rotate.d
+++
@@ -1,6 +0,0 @@
-.make/build_main/moves/reverse_rotate.o: src/moves/reverse_rotate.c \
-  include/struct.h include/move.h
-
-include/struct.h:
-
-include/move.h:
